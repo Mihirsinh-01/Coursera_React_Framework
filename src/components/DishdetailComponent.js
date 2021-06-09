83bb0820a00c89b956a8d60271eee0d4269d1cbc
Mihirsinh <vajamihir20@gmail.com>
2021-06-09 16:48:11 +0530

    Week 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardTitle, Breadcrumb,BreadcrumbItem  } from 'reactstrap';
 import { format } from 'date-fns';
+import { Link } from 'react-router-dom';
 
 
 function RenderDish({ dish }) {
@@ -44,8 +45,22 @@ const DishDetail = (props) => {
         return (
             <div className="container">
                 <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to='/menu'>Menu</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            {props.dish.name}
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
                     <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments} />
+                    <RenderComments comments={props.comments} />
                 </div>
             </div>
         );
