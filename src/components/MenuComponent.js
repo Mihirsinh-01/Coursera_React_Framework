5d4ddd83ae347a42e62adeaff43dd6b25979ce68
Mihirsinh <vajamihir20@gmail.com>
2021-06-15 16:55:12 +0530

    Redux Thunk

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Loading } from './LoadingComponent';
 
 function RenderMenuItem({dish}) {
     return (
@@ -17,7 +17,7 @@ function RenderMenuItem({dish}) {
 }
 const Menu = (props) => {
     
-    const menu = props.dishes.map((dish) => {
+    const menu = props.dishes.dishes.map((dish) => {
         return (
             <div key={dish.id} className="col-12 col-md-5 m-1">
                 <RenderMenuItem dish={dish}/>
@@ -25,28 +25,50 @@ const Menu = (props) => {
         );
     });
 
-
-    return (            
+    if (props.dishes.isLoading) {
+        return (
         <div className="container">
             <div className="row">
-                <Breadcrumb>
-                    <BreadcrumbItem>
-                        <Link to='/home'>Home</Link>
-                    </BreadcrumbItem>
-                    <BreadcrumbItem active>
-                        Menu
-                    </BreadcrumbItem>
-                </Breadcrumb>
-                <div className="col-12">
-                    <h3>Menu</h3>
-                    <hr />
-                </div>
+            <Loading />
             </div>
+        </div>
+        );
+    }
+    else if (props.dishes.errMess) {
+        return (
+        <div className="container">
             <div className="row">
-                {menu}
+            <h4>{props.dishes.errMess}</h4>
             </div>
-        </div >
-    );
+        </div>
+        );
+    }
+    else {
+        return (            
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem>
+                            <Link to='/home'>Home</Link>
+                        </BreadcrumbItem>
+                        <BreadcrumbItem active>
+                            Menu
+                        </BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>Menu</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="row">
+                    {menu}
+                </div>
+            </div >
+        );
+    }
+
+
+    
 }
 
 export default Menu;
